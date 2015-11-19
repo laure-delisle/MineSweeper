3094bef79eaddd8c0c027f42f95b8608f46bfbd1
laure-delisle <laure.delisle@gmail.com>
2015-11-19 04:26:13 +0100

    Display grid, handle defeat

--- Game.java
+++ Game.java
@@ -1,7 +1,7 @@
-/* importations */
-import java.util.Scanner;
 import java.lang.System;
 import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Game
 {
@@ -15,6 +15,8 @@ public class Game
 	private int totalCellNumber;
 	private int coveredNonminedCellNumber;
 	private int grid[][];
+	private Boolean isTheEnd;
+	private int x,y;
 
 	public Game()
 	{
@@ -22,13 +24,14 @@ public class Game
 		width = 0;
 		height = 0;
 		mineNumber = 0;
+		isTheEnd = false;
 
 		//get width, height and number of mines from user through console
-		width = getInt("Width >0");
-		height = getInt("Height >0");
+		width = getInt("Width 0<...<=50 : ",50);
+		height = getInt("Height 0<...<=20 : ",20);
 		totalCellNumber = width*height;
 		do
-			mineNumber = getInt("Number of mines 0<...<" + totalCellNumber);
+			mineNumber = getInt("Number of mines 0<...<"+totalCellNumber+" : ",totalCellNumber);
 		while (totalCellNumber <= mineNumber);
 
 		//compute the number of non-mined cells
@@ -36,18 +39,35 @@ public class Game
 
 		//create grid and fill it with mines
 		grid = new int[height][width];
-		fillGrid(width,height,grid,mineNumber);
+		fillGrid();
+
+		//step : display grid and ask user for his next move
+		while (false == isTheEnd)
+		{
+			displayGrid();
+			System.out.println("Please choose a cell to uncover [lin col] : ");
+			x = getInt("", height-1);
+			y = getInt("", width-1);
+			step();
+		}
+		System.out.println("Game is over.");
 	}
 
-	private int getInt(String varDescriptor)
+	public static void clearScreen()
+	{
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
+	private int getInt(String varDescriptor, int threshold)
 	{
 		int varToSet = 0;
 
-		while (0 >= varToSet)
+		while ((0 >= varToSet) || (threshold < varToSet))
 		{
 			try
 			{
-				System.out.print(varDescriptor+ " : ");
+				System.out.print(varDescriptor);
 				varToSet = scanner.nextInt();
 			}
 			catch (InputMismatchException e) {
@@ -60,9 +80,72 @@ public class Game
 		return (varToSet);
 	}
 
-	private void fillGrid(int width, int height, int grid[][], int mineNumber)
+	private void fillGrid()
 	{
-		//to do
+		Random random = new Random();
+		int randCol = 0;
+		int randLin = 0;
+		int i,j;
+		int minesToPlace = mineNumber;
+		
+		//init grid with unknown cells :
+		for (i=0; i<height; i++)
+		{
+			for (j=0; j<width; j++)
+			{
+				grid[i][j] = UNKNOWN_CELL;
+			}
+		}
 
+		//insert mines
+		while (0 < minesToPlace)
+		{
+			randCol = random.nextInt(width);
+			randLin = random.nextInt(height);
+			if (UNKNOWN_CELL == grid[randLin][randCol])
+			{
+				grid[randLin][randCol] = MINE_CELL;
+				minesToPlace--;
+			}
+		}
 	}
+
+	private void displayGrid()
+	{
+		int i,j;
+		clearScreen();
+
+		//horizontal axis legend
+		System.out.print("  ");
+		for (j=0; j<width; j++) System.out.print(String.format("%3d", j));
+		System.out.print("\n   ");
+		for (j=0; j<3*width; j++) System.out.print("_");
+		System.out.print("\n");
+
+		//vertical axis legend + grid
+		for (i=0; i<height; i++)
+		{
+			System.out.print(i+" | ");
+			for (j=0; j<width; j++)
+			{
+				if (UNKNOWN_CELL == grid[i][j]) System.out.print(".");
+				else if ((MINE_CELL == grid[i][j]) && (false==isTheEnd)) System.out.print(".");
+				else if ((MINE_CELL == grid[i][j]) && (true==isTheEnd)) System.out.print("X");
+				else System.out.print(grid[i][j]);
+				System.out.print("| ");
+			}
+			System.out.println("");
+		}
+	}
+
+	private void step()
+	{
+		if (MINE_CELL == grid[x][y])
+		{
+			isTheEnd = true;
+			displayGrid();
+			System.out.println("There was a mine at ["+x+","+y+"]...\nYou loose !");
+		}
+	}
+
 }
